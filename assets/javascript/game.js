68ee978f268fe5ec1bfbd2a52997adc7301b6600
Joe <dinhx068@umn.edu>
2018-07-28 00:49:35 -0500

    some more progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+// Global variables
+var enemySelected = false;
+
 $(document).ready(function() {
 
 jQuery.fn.visible = function() {
@@ -14,12 +17,29 @@ jQuery.fn.visibilityToggle = function() {
     });
 };
 
-$(".image-container").on("click", function() {
+// When th user clicks on one of the starting characters
+$(".selection").one("click", function() {
     console.log("Did this work?");
+    $("#characters").text("Character Chosen");
     $(".evil-goblin").visible();
     $(".enemy-list").visible();
     $("#log-container").visible();
     $(".log-line-1").text("Choose an enemy to attack");
+    // console.log($(".slayer-hp").text() - 5); Gets hp
+    $(".slayer-hp").text($(".slayer-hp").text() - 5);
+})
+
+// When we select the first enemy to attack
+$(".goblin_1").on("click", function() {
+    // If there is no enemy selected then we move the goblin into position
+    if (!enemySelected) {
+    var $cloneGoblin = $(".goblin_1").clone();
+    $(".container-to-clone").html($cloneGoblin);
+    enemySelected = true; // So we don't fight mutiple enemies
+    } else {
+        $(".log-line-1").text("You're already an enemy");
+        $(".log-line-2").text("You can either attack or run away");
+    }
 })
 
 });
